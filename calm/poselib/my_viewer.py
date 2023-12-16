f9a7544ccb91591ec147f63652fe266e2910b3f8
Ivan <chenyifei9@live.com>
2023-12-16 15:20:40 +0800

    poselib viewer

--- calm/poselib/my_viewer.py
+++ calm/poselib/my_viewer.py
@@ -4,10 +4,10 @@ from poselib.skeleton.skeleton3d import SkeletonMotion, SkeletonState
 
 def main():
     # pose = SkeletonState.from_file("data/cmu_tpose.npy")
-    # motion = SkeletonMotion.from_file("data/RL_Avatar_WalkRight02_Motion.npy")
-    motion = SkeletonMotion.from_fbx("mydata/D1_013_KAN01_001.fbx")
+    motion = SkeletonMotion.from_file("mydata/RL_Avatar_WalkRight02_Motion.npy")
+    # motion = SkeletonMotion.from_fbx("mydata/D1_013_KAN01_001.fbx")
     # save motion
-    motion.to_file("mydata/D1_013_KAN01_001.npy")
+    # motion.to_file("mydata/D1_013_KAN01_001.npy")
     plot_skeleton_motion_interactive(motion)
     
 
